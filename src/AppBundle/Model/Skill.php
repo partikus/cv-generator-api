3bfaf7111d4f39213d1304c63cf2135e7632028d
Michal Szczur <michal@clearcode.eu>
2016-09-17 14:06:41 +0200

    Added description and url fields to BaseSkill model

--- src/AppBundle/Model/Skill.php
+++ src/AppBundle/Model/Skill.php
@@ -7,9 +7,37 @@ namespace AppBundle\Model;
 class Skill
 {
     private $name;
+    private $description;
+    private $url;
 
-    public function __construct(string $name)
+    public function __construct(string $name, string $description, string $url)
     {
         $this->name = $name;
+        $this->description = $description;
+        $this->url = $url;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
     }
 }
